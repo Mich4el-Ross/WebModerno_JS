482962304aa8576bb723ddee548e391425f8c835
Michael <michael.doug@outlook.com>
2021-10-21 20:59:09 -0300

    Exercício 003

---
+++ 01_Fundamentos/ex003.js
@@ -0,0 +1,11 @@
+/*
+Exercício 003:
+
+Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+*/
+
+function potencia(base, expoente){
+    return base ** expoente
+}
+
+console.log(potencia(5, 2))
