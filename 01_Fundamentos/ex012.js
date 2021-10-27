52622636b2c07b7d27256ea410bf4b961ca91b8a
Michael <michael.doug@outlook.com>
2021-10-26 21:26:39 -0300

    Exercício 012

---
+++ 01_Fundamentos/ex012.js
@@ -0,0 +1,16 @@
+/*
+Exercício 012:
+
+Faça um algoritmo que calcule o fatorial de um número.
+*/
+
+function fatorial(num){
+
+    if(num == 0){
+        return 1;
+    } else{
+        return num * fatorial(num - 1);
+    }
+} 
+
+console.log(fatorial(5));
