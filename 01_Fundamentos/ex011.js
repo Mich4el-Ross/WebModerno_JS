ca2fdfc5be0f210ddd3be9dce06d0a9a25c6e690
Michael <michael.doug@outlook.com>
2021-10-26 21:31:27 -0300

    Exercício 011

--- 01_Fundamentos/ex011.js
+++ 01_Fundamentos/ex011.js
@@ -7,7 +7,9 @@ As regras para o cálculo dos anos bissextos são as seguintes:
     - De 100 em 100 anos não é ano bissexto;
     - De 400 em 400 anos é ano bissexto;
 
-Lembrando que prevalecem as últimas regras sobre as primeiras elabore uma função que recebe um ano e calcula se ele é ano bissexto, imprimindo no console a mensagem e retornando true ou false.
+Lembrando que prevalecem as últimas regras sobre as primeiras elabore uma 
+função que recebe um ano e calcula se ele é ano bissexto, imprimindo no 
+console a mensagem e retornando true ou false.
 */ 
 
 function anoBissexto(ano){
