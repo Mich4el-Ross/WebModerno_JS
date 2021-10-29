4480aeba6f725bf687550fc07b3c6b4cbc2b8cfb
Michael <michael.doug@outlook.com>
2021-10-29 20:49:35 -0300

    Exercício 025

---
+++ 01_Fundamentos/ex025.js
@@ -0,0 +1,14 @@
+/*
+Exercício 025:
+
+Escrever um programa para exibir os números de 1 até 50 na tela.
+*/
+
+function contaCinquenta(){
+
+    for (let cont = 1; cont <= 50; cont++){
+        console.log(cont);
+    }
+}
+
+contaCinquenta()
