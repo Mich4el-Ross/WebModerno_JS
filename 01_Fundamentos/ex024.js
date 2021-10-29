804161854e8a2f167cd3a22ed60608b6d4341e32
Michael <michael.doug@outlook.com>
2021-10-29 20:49:29 -0300

    Exercício 024

---
+++ 01_Fundamentos/ex024.js
@@ -0,0 +1,18 @@
+/*
+Exercício 024:
+
+Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma 
+estrutura de repetição while.
+*/
+
+function helloWorld(){
+
+    let cont = 1
+
+    while (cont <= 11 ) {
+        console.log('Hello World!')
+        cont++
+    }
+}
+
+helloWorld()
