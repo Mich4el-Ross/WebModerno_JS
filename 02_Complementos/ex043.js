8827aa6b78016a580d59e596f0b79ee89c037cd2
Michael <michael.doug@outlook.com>
2021-11-16 21:28:26 -0300

    Exercício 043

---
+++ 02_Complementos/ex043.js
@@ -0,0 +1,14 @@
+/*
+Exercício 043:
+
+Desenvolva uma função que recebe dois parâmetros, um é a quantidade de horas trabalhadas por um funcionário 
+num mês, e o quanto ele recebe por hora. O retorno da função deve ser a string "Salário igual a R$X", em 
+que X é o quanto o funcionário ganhou no mês.
+*/
+
+function calculoSalario(horas, valorHora) {
+    let salario = horas * valorHora 
+    console.log(`Salário igual a R$ ${salario}`)
+}
+
+calculoSalario(175, 45.5)
