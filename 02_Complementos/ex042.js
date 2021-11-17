aab23863368988c28abce223acf55c4190eb4ff0
Michael <michael.doug@outlook.com>
2021-11-16 21:28:20 -0300

    Exercício 042

---
+++ 02_Complementos/ex042.js
@@ -0,0 +1,14 @@
+/*
+Exercício 042:
+
+Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias
+
+OBS: Considere que um ano tem 365 dias. Desconsidere anos bissextos (com 366 dias) e
+desconsidere também dias decorridos desde o último aniversário
+*/
+
+function converterIdade(idade){
+    console.log(idade * 365)
+}
+
+converterIdade(37)
