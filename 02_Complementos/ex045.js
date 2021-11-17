dd7dd4cdffdc22b345fd3f5f08f5763ff956ef31
Michael <michael.doug@outlook.com>
2021-11-16 21:28:44 -0300

    Exercício 045

---
+++ 02_Complementos/ex045.js
@@ -0,0 +1,12 @@
+/*
+Exercício 045:
+
+Crie uma função que receba dois números e retorne se o primeiro é maior ou igual ao segundo.
+*/
+
+function maiorOuIgual(num1, num2){
+    return `O resultado é ${num1 >= num2}`
+}
+
+console.log(maiorOuIgual(2, 4))
+console.log(maiorOuIgual('7', 3))
